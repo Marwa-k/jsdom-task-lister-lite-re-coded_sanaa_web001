848eb7cc2ffc917110fe02e745b9126bdf7f54da
Marwa Alaghbri <marwaalaghbry@gmail.com>
2020-09-06 16:44:11 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
 document.addEventListener("DOMContentLoaded", function(event) => {
   // your code here
-  event.preventDefault();
+  
+  document.addEventListener("submit", function(event) {
+    event.preventDefault()
+    let listOftasks = document.getElementById("tasks");
+    let newTask = document.getElementById("new-task-description");
+    let li = document.createElement("li");
+    let text=document.createTextNode(newTask.value)
+    li.appendChild(text);
+    listOftasks.appendChild(li);
+    event.target.reset();
 });
