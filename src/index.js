01a38cd9bc0ec5eca8ea101684bc49738c41e2f3
Marwa Alaghbri <marwaalaghbry@gmail.com>
2020-09-06 16:52:05 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded",() => {
   // your code here
-});
+    let listOftasks = document.getElementById("tasks");
+  document.addEventListener("submit", function(event) {
+    event.preventDefault()
+    let newTask = document.getElementById("new-task-description");
+    let li = document.createElement("li");
+    let text=document.createTextNode(newTask.value)
+    li.appendChild(text);
+    listOftasks.appendChild(li);
+    event.target.reset();
+})
+})
